771ad349cac19486730ee3f3337ca6640ddcb30a
Jacob Partusch <jakepartusch@gmail.com>
2020-01-06 10:30:43 -0600

    feat: basic logic to wait for a 200 response

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const axios = require("axios");
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
+  const url = core.getInput("url");
+  console.log(`Waiting for a 200 from: ${url}`);
+
+  for (let i = 0; i < 5; i++) {
+    try {
+      await axios.get(url);
+    } catch (e) {
+      await new Promise(r => setTimeout(r, 5000));
+      if (i === 4) {
+        core.setFailed(`Timeout reached: Unable to connect to ${url}`);
+      }
+    }
+  }
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
